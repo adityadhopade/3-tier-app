bdde90b8ef90ddd8189b32d6b69c624643627479
adityadhopade <adityadhopade18@gmail.com>
2023-06-15 19:52:46 +0530

    Added the route tables and internet gateway for public route table

--- network.tf
+++ network.tf
@@ -20,6 +20,41 @@ resource "aws_subnet" "private_subnet" {
 }
 
 
+resource "aws_default_route_table" "this" {
+  default_route_table_id = aws_vpc.this.default_route_table_id
+
+  # WE do not need the route for the local by default it will be added
+
+  tags = {
+    Name = "private_routetable_${var.vpc_name}"
+  }
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  # WE do not need the route for the local by default it will be added
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    # in public rt we require the internet gateway also
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "public_routetable_${var.vpc_name}"
+  }
+}
+
+#We need to add it and what arguments are required in it with the attribute refrences
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "igw-${var.vpc_name}"
+  }
+}
 resource "aws_subnet" "public_subnet" {
   for_each          = { for index, az_name in slice(data.aws_availability_zones.this.names, 0, 2) : index => az_name }
   vpc_id            = aws_vpc.this.id
