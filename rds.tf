164dd8c3d478e4cb1d10f3d3c695349574904ed9
adityadhopade <adityadhopade18@gmail.com>
2023-07-28 17:50:49 +0530

    code added for rds, chnagesin provison.yml

---
+++ rds.tf
@@ -0,0 +1,21 @@
+resource "aws_db_instance" "default" {
+  allocated_storage      = 10
+  db_name                = "threetierdb"
+  engine                 = "mysql"
+  engine_version         = "8.0.32"
+  instance_class         = "db.t3.micro"
+  username               = var.db_user_name
+  password               = var.db_password
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+}
+
+resource "aws_db_subnet_group" "this" {
+  name = "threetierdb_subnet_group"
+  #subnets should lie into the private subnets so we need to add private subnets from the app_server.tf
+  subnet_ids = [for each_subnet in aws_subnet.private_subnet : each_subnet.id]
+
+  tags = {
+    Name = "threetier_db_subnet"
+  }
+}
